8ebb92e0bc0897c1af1687584abc3520d0c30273
marc_duda <marcin.duda@alcon.com>
2023-05-16 10:30:15 +0200

    feat: add ssr to categories page

--- pages/categories.tsx
+++ pages/categories.tsx
@@ -2,14 +2,31 @@ import Page from "@/components/templates/Page/Page";
 import NavigationBar from "@/components/organisms/NavigationBar/NavigationBar";
 import styles from "./categories.module.css";
 import Tile from "@/components/atoms/Tile/Tile";
-import useSWR from 'swr'
-import { fetcher } from "@/utils/fetcher";
 import { Category } from "@prisma/client";
 import { useIntl } from "react-intl";
 
-const Categories: React.FunctionComponent = () => {
-    const { data, error } = useSWR('/api/categories', fetcher)
+export async function getServerSideProps() {
+    const response = await fetch('http://localhost:3000/api/categories')
 
+    if (!response) {
+        return { props: {} }
+    }
+
+    const categories = await response.json();
+
+    return {
+        props: {
+            categories,
+        }
+    }
+}
+
+export interface CategoriesProps {
+    categories: Category[];
+}
+
+const Categories: React.FunctionComponent<CategoriesProps> = (props) => {
+    const { categories } = props;
     const intl = useIntl();
     const title = intl.formatMessage({ id: "page.categories.title" });
     const subtitle = intl.formatMessage({ id: "page.categories.description" });
@@ -21,9 +38,7 @@ const Categories: React.FunctionComponent = () => {
                 subtitle={subtitle}
             >
                 <div className={styles.container}>
-                    {error ? <div>An error occured.</div> : null}
-
-                    {data ? data.map((category: Category, index: number) => <Tile
+                    {categories ? categories.map((category: Category, index: number) => <Tile
                         key={category.id}
                         type="primary"
                         category={category}
